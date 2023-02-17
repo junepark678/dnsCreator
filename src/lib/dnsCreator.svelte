afcad762ca53b86d1d61ff55e4ceeb8162f5a12a
June P <40409848+junepark678@users.noreply.github.com>
2023-02-18 02:19:36 +0900

    make more modular

--- src/lib/dnsCreator.svelte
+++ src/lib/dnsCreator.svelte
@@ -1,81 +1,103 @@
 <script lang="ts">
 import { v4 as uuidv4 } from 'uuid';
+// import * as base_profile from '../../public/base_profile.json';
+import { parse, serialize } from '@plist/plist';
+import { serialize as serializeXML } from '@plist/xml.serialize';
+
 let config: string;
 let ip: string = "";
 let ip2: string = "";
 let blob: Blob;
 let url: string;
 let DoH: string;
-let generate = () => {
+let generate = async () => {
     if(ip === ""){
         return;
     }
-    config = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-	<dict>
-		<key>PayloadDisplayName</key>
-		<string>Custom DNS: ${ip}</string>
-		<key>PayloadOrganization</key>
-		<string>junengames.ml</string>
-		<key>PayloadDescription</key>
-		<string>This profile adds a custom DNS to your iDevice</string>
-		<key>PayloadIdentifier</key>
-		<string>${uuidv4()}</string>
-		<key>PayloadScope</key>
-		<string>User</string>
-		<key>PayloadType</key>
-		<string>Configuration</string>
-		<key>PayloadUUID</key>
-		<string>${uuidv4()}</string>
-		<key>PayloadVersion</key>
-		<integer>1</integer>
-		<key>PayloadContent</key>
-		<array>
-			<dict>
-				<key>DNSSettings</key>
-				<dict>
-					<key>DNSProtocol</key>
-					<string>HTTPS</string>
-					<key>ServerAddresses</key>
-					<array>
-						<string>${ip}</string>
-                        <string>${ip2}</string>
-					</array>
-                    <key>ServerURL</key>
-					<string>${DoH}</string>
-				</dict>
-				<key>PayloadType</key>
-				<string>com.apple.dnsSettings.managed</string>
-				<key>PayloadIdentifier</key>
-				<string>${uuidv4()}</string>
-				<key>PayloadUUID</key>
-				<string>${uuidv4()}</string>
-				<key>PayloadDisplayName</key>
-				<string>Custom DNS: ${ip}</string>
-				<key>PayloadVersion</key>
-				<integer>1</integer>
-			</dict>
-		</array>
-		<key>OnDemandRules</key>
-		<array>
-			<dict>
-				<key>Action</key>
-				<string>Connect</string>
-				<key>InterfaceTypeMatch</key>
-				<string>Cellular</string>
-			</dict>
-			<dict>
-				<key>Action</key>
-				<string>Connect</string>
-				<key>URLStringProbe</key>
-				<string>http://captive.apple.com/hotspot-detect.html</string>
-			</dict>
-		</array>
-	</dict>
-</plist>
-`;
-    blob = new Blob([config], {type: "application/x-apple-aspen-config"})
+
+    var base_profile = await fetch("/base_profile.json");
+    var new_profile = await base_profile.json();
+    // var new_profile = base_profile.json();
+
+    // console.log(serializeXML(base_profile))
+
+    new_profile.PayloadDisplayName = `Custom DNS: ${ip}`;
+    new_profile.PayloadIdentifier = `${uuidv4()}`;
+    new_profile.PayloadUUID = `${uuidv4()}`;
+    new_profile.PayloadContent[0].DNSSettings.ServerAddresses[0] = ip;
+    new_profile.PayloadContent[0].DNSSettings.ServerAddresses[1] = ip2;
+    new_profile.PayloadContent[0].DNSSettings.ServerURL = DoH;
+    new_profile.PayloadContent[0].PayloadIdentifier = `${uuidv4()}`;
+    new_profile.PayloadContent[0].PayloadUUID = `${uuidv4()}`;
+    new_profile.PayloadContent[0].PayloadDisplayName = `Custom DNS: ${ip}`;
+
+
+//     config = `<?xml version="1.0" encoding="UTF-8"?>
+// <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+// <plist version="1.0">
+// 	<dict>
+// 		<key>PayloadDisplayName</key>
+// 		<string>Custom DNS: ${ip}</string>
+// 		<key>PayloadOrganization</key>
+// 		<string>junengames.ml</string>
+// 		<key>PayloadDescription</key>
+// 		<string>This profile adds a custom DNS to your iDevice</string>
+// 		<key>PayloadIdentifier</key>
+// 		<string>${uuidv4()}</string>
+// 		<key>PayloadScope</key>
+// 		<string>User</string>
+// 		<key>PayloadType</key>
+// 		<string>Configuration</string>
+// 		<key>PayloadUUID</key>
+// 		<string>${uuidv4()}</string>
+// 		<key>PayloadVersion</key>
+// 		<integer>1</integer>
+// 		<key>PayloadContent</key>
+// 		<array>
+// 			<dict>
+// 				<key>DNSSettings</key>
+// 				<dict>
+// 					<key>DNSProtocol</key>
+// 					<string>HTTPS</string>
+// 					<key>ServerAddresses</key>
+// 					<array>
+// 						<string>${ip}</string>
+//                         <string>${ip2}</string>
+// 					</array>
+//                     <key>ServerURL</key>
+// 					<string>${DoH}</string>
+// 				</dict>
+// 				<key>PayloadType</key>
+// 				<string>com.apple.dnsSettings.managed</string>
+// 				<key>PayloadIdentifier</key>
+// 				<string>${uuidv4()}</string>
+// 				<key>PayloadUUID</key>
+// 				<string>${uuidv4()}</string>
+// 				<key>PayloadDisplayName</key>
+// 				<string>Custom DNS: ${ip}</string>
+// 				<key>PayloadVersion</key>
+// 				<integer>1</integer>
+// 			</dict>
+// 		</array>
+// 		<key>OnDemandRules</key>
+// 		<array>
+// 			<dict>
+// 				<key>Action</key>
+// 				<string>Connect</string>
+// 				<key>InterfaceTypeMatch</key>
+// 				<string>Cellular</string>
+// 			</dict>
+// 			<dict>
+// 				<key>Action</key>
+// 				<string>Connect</string>
+// 				<key>URLStringProbe</key>
+// 				<string>http://captive.apple.com/hotspot-detect.html</string>
+// 			</dict>
+// 		</array>
+// 	</dict>
+// </plist>
+// `;
+    blob = new Blob([serializeXML(new_profile)], {type: "application/x-apple-aspen-config"})
     
     url = URL.createObjectURL(blob);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteRequire } from 'vite-require'
+import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), viteCommonjs(), ],
+
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        esbuildCommonjs(['simple-plist', 'stream-buffers']),
+      ],
+    },
+  },
 })
